5b92e637d3021eb15da8ba895b6a60e8260a9dee
ysiin <zachbone0@gmail.com>
2020-03-15 10:32:06 +0700

    perbarui halaman

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ function login() {
     const name = $("#userName").val()
     if (name) {
         localStorage.setItem('name', name)
-        window.location.href = "./homepage.html"
+        window.location.href = "./index.html"
     }
-}
-
-
-
-function handleOnChange() {
+  }
+  
+  
+  
+  function handleOnChange() {
     console.log('called')
     const name = $("#userName").val()
     const pass = $("#password").val()
     setTimeout(function () {
-            console.log('kepanggil setelah 2  ms')
         if (name && pass) {
             $("#loginBtn").removeClass(['disabled'])
         } else if (!$("#loginBtn").hasClass('disabled')) {
@@ -22,12 +21,24 @@ function handleOnChange() {
             $("#loginBtn").addClass(['disabled'])
         }
     },200)
-}
-
-$(document).ready(function(){
+  }
+  
+  $(document).ready(function(){
     console.log('load')
     const name = localStorage.getItem('name')
-    console.log(name);
-    $("#greet").text(`Halo ${name}!`)
-    
-})
+    console.log(window.location.pathname)
+    const isHomepage = window.location.pathname === '/'
+    const isLoginPage = window.location.pathname === '/masuk.html'
+    // auth guard
+    if (isHomepage) {
+        if (name) {
+            $("#greet").text(`Halo ${name}!`)
+        } else {
+            window.location.href = 'masuk.html'
+        }
+    } else if (isLoginPage) {
+        if (name) {
+            window.location.href = '/'
+        }
+    }
+  })
